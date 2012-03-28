4aa85623d41d306c437fccc1d6dc2a6a6c617d73
David Elliott <david@hadto.net>
2012-03-28 14:19:25 -0700

    noticed a problem with the main nav centering

--- footer.php
+++ footer.php
@@ -5,11 +5,11 @@
 						<?php bones_footer_links(); // Adjust using Menus in Wordpress Admin ?>
 					</nav>
 				
-					<p class="attribution">The 2012 UCLA Game Art Festival is a project of the UCLA Game Lab. Sponsored by the School of Arts and Architecture and the School of Theater, Film and Television</p>
-					<div id="HAMMER" class="footer-logo"><a href="http://hammer.ucla.edu/">The Hammer Museum</a></div>
-					<div id="HSA" class="footer-logo"><a href="http://hammer.ucla.edu/about/hsa.html">Hammer Student Association</a></div>
-					<div id="Arts" class="footer-logo"><a href="http://www.arts.ucla.edu/">School of Arts and Architecture</a></div>
-					<div id="TFT" class="footer-logo"><a href="http://www.tft.ucla.edu/">School of Theater, Film, Television</a></div>
+					<p class="attribution fourcol">The 2012 UCLA Game Art Festival is a project of the UCLA Game Lab. Sponsored by the School of Arts and Architecture and the School of Theater, Film and Television</p>
+					<div id="HAMMER" class="footer-logo twocol"><a href="http://hammer.ucla.edu/">The Hammer Museum</a></div>
+					<div id="HSA" class="footer-logo twocol"><a href="http://hammer.ucla.edu/about/hsa.html">Hammer Student Association</a></div>
+					<div id="Arts" class="footer-logo twocol"><a href="http://www.arts.ucla.edu/">School of Arts and Architecture</a></div>
+					<div id="TFT" class="footer-logo twocol"><a href="http://www.tft.ucla.edu/">School of Theater, Film, Television</a></div>
 
 
 				</div> <!-- end #inner-footer -->
