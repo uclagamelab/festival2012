06375e07ac70bb2e23a748221ec101b29441e5cc
David Elliott <david@hadto.net>
2012-03-28 16:25:34 -0700

    Started working on the front page template layout
    Looking good so far. Grid works fine. Need to add some more real
    content to further build, will use last years content. Also need to
    change the footer logos to use the grid and be siblings with the credit
    text

---
+++ dev-page.php
@@ -0,0 +1,183 @@
+<?php
+/*
+Template Name: Dev Page
+*/
+?>
+
+<?php get_header(); ?>
+			
+			<div id="content">
+			
+				<div id="inner-content" class="wrap clearfix">
+			
+					<div id="main" class="twelvecol clearfix" role="main">
+						<section id="featured" class="row">
+							<article id="featured-post-1" class="twelvecol" role="article">
+								
+								<header>
+
+									<h1>This is the first featured post</h1>
+								
+								</header> <!-- end article header -->
+							
+								<section class="post-content">
+									<a href="#" class="thumbnail">
+										<img src="http://placehold.it/914x307" alt="">
+									</a>
+									And here is some post content
+							
+								</section> <!-- end article section -->
+								
+								<footer>
+							
+									<p class="clearfix">footer stuff goes here</p>
+									
+								</footer> <!-- end article footer -->
+							
+							</article> <!-- end article -->
+						</section><!-- end #featued -->
+						<section id="sub-featured" class="row">
+
+							<article id="post-1" class="sixcol" role="article">
+								
+								<header>
+
+									<h1>This is the first sub featued post</h1>
+								
+								</header> <!-- end article header -->
+							
+								<section class="post-content">
+									<a href="#" class="thumbnail">
+										<img src="http://placehold.it/444x307" alt="">
+									</a>
+									And here is some post content
+							
+								</section> <!-- end article section -->
+								
+								<footer>
+						
+									<p class="clearfix">footer stuff goes here</p>
+									
+								</footer> <!-- end article footer -->
+							
+							</article> <!-- end article -->
+
+							<article id="post-2" class="sixcol last" role="article">
+								
+								<header>
+
+									<h1>This is the second sub featued post</h1>
+								
+								</header> <!-- end article header -->
+							
+								<section class="post-content">
+									<a href="#" class="thumbnail">
+										<img src="http://placehold.it/444x307" alt="">
+									</a>
+									And here is some post content
+							
+								</section> <!-- end article section -->
+								
+								<footer>
+							
+									<p class="clearfix">footer stuff goes here</p>
+									
+								</footer> <!-- end article footer -->
+							
+							</article> <!-- end article -->
+						</section><!-- end #sub-featured -->
+						<section id="all-post-grid" class="row">
+							<article id="grid-post-1" class="sixcol" role="article">
+								
+								<header>
+
+									<h1>Lesser important people</h1>
+								
+								</header> <!-- end article header -->
+							
+								<section class="post-content">
+									<a href="#" class="thumbnail">
+										<img src="http://placehold.it/94x94" alt="" class="alignleft">
+									</a>
+									And here is some post content. Need to add a bit more text to fill up this paragraph. This will be a great event. Really looking forward to it! And I can't wait for the after party as well!
+							
+								</section> <!-- end article section -->
+								
+								<footer>
+																
+								</footer> <!-- end article footer -->
+							
+							</article> <!-- end article -->
+							<article id="grid-post-2" class="sixcol last" role="article">
+								
+								<header>
+
+									<h1>Lesser important people</h1>
+								
+								</header> <!-- end article header -->
+							
+								<section class="post-content">
+									<a href="#" class="thumbnail">
+										<img src="http://placehold.it/94x94" alt="" class="alignleft">
+									</a>
+									And here is some post content. Need to add a bit more text to fill up this paragraph. This will be a great event. Really looking forward to it! And I can't wait for the after party as well!
+							
+								</section> <!-- end article section -->
+								
+								<footer>
+																
+								</footer> <!-- end article footer -->
+							
+							</article> <!-- end article -->	
+							<article id="grid-post-1" class="sixcol" role="article">
+								
+								<header>
+
+									<h1>Lesser important people</h1>
+								
+								</header> <!-- end article header -->
+							
+								<section class="post-content">
+									<a href="#" class="thumbnail">
+										<img src="http://placehold.it/94x94" alt="" class="alignleft">
+									</a>
+									And here is some post content. Need to add a bit more text to fill up this paragraph. This will be a great event. Really looking forward to it! And I can't wait for the after party as well!
+							
+								</section> <!-- end article section -->
+								
+								<footer>
+																
+								</footer> <!-- end article footer -->
+							
+							</article> <!-- end article -->
+							<article id="grid-post-2" class="sixcol last" role="article">
+								
+								<header>
+
+									<h1>Lesser important people</h1>
+								
+								</header> <!-- end article header -->
+							
+								<section class="post-content">
+									<a href="#" class="thumbnail">
+										<img src="http://placehold.it/94x94" alt="" class="alignleft">
+									</a>
+									And here is some post content. Need to add a bit more text to fill up this paragraph. This will be a great event. Really looking forward to it! And I can't wait for the after party as well!
+							
+								</section> <!-- end article section -->
+								
+								<footer>
+																
+								</footer> <!-- end article footer -->
+							
+							</article> <!-- end article -->	
+
+						</section> <!-- end #all-post-grid -->
+
+					</div> <!-- end #main -->
+    									
+				</div> <!-- end #inner-content -->
+    
+			</div> <!-- end #content -->
+
+<?php get_footer(); ?>
