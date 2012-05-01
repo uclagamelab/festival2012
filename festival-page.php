44e68d940b4a364efd09288291f4cd05da3116b2
David Elliott <david@hadto.net>
2012-05-01 10:17:00 -0700

    updated the dates and locations to be consolidated

--- festival-page.php
+++ festival-page.php
@@ -15,20 +15,17 @@ Template Name: Festival Page
 							<blockquote>A two day festival of experimental games, demos, game art, performances, music and tournaments. Admission is free and open to the public!</blockquote>
 						</section>
 						<hr/>
-						<section id="when">
-							<h1>Dates</h1>
-							<p><span class="time">May 9th from 7 to 10pm</span> at the Hammer Museum</p>
-							<p><span class="time">May 10th from 6 to 10pm</span> at the Broad Art Center, UCLA</p>
-						</section>
-						<hr/>
-						<section id="where">
-							<h1>Directions</h1>
-							<p id="hammer-parking">
-								The Hammer Museum is located at the northeast corner of Westwood and Wilshire Boulevards in Westwood Village, 3 blocks east of the 405 freeway's Wilshire Boulevard exit. <a href="http://maps.google.com/maps?hl=en&q=10899+Wilshire+Blvd+90024">View Map</a>
-							</p><!-- hammer-parking -->
-							<p id="broad-parking">
-								Directions and parking info for the Broad Art Center, UCLA can be found here: <a href="http://dma.ucla.edu/information/directions/">http://dma.ucla.edu/information/directions/</a>
-							</p><!-- broad-parking -->
+						<section id="locations" class="twelvecol">
+							<article id="location-hammer" class="sixcol clearfix">
+								<header><h1><span class="time">May 9th from 7 to 10pm</span><br/> at the Hammer Museum</h1></header>
+								<p>10899 Wilshire Boulevard<br/> Los Angeles, CA 90024</p>
+								<p>Parking is available under the Museum. Rates are $3 for the first three hours with Museum stamp; $1.50 for each additional 15 minutes. There is a $3 flat rate after 6:00 p.m.. There is a $3 flat rate on Saturdays and Sundays.  Parking for people with disabilities is provided on levels P1 and P3. <a href="http://hammer.ucla.edu/visit/index.html">More info about getting to the Hammer</a></p>
+							</article>
+							<article id="location-broad" class="sixcol last clearfix">
+								<header><h1><span class="time">May 10th from 6 to 10pm</span><br/> at the Broad Art Center EDA, UCLA<h1></header>
+								<p>240 Charles E. Young Dr.<br/> Room 1250<br/> Los Angeles CA</p>
+								<p>The EDA (Room 1250) is located on the first floor of the Broad Art Center. All-day parking ($11) and short-term parking (payable at pay stations) are available in Lot 3 (enter the campus at Hilgard and Westholme avenues). <a href="http://dma.ucla.edu/information/directions/">Directions to the Broad Art Center</a></p>
+							</article>
 						</section>
 						<hr/>
 						<section id="who" class="row">
